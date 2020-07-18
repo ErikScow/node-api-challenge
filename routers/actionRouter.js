dc44639a3106a325df1b04389e56244a0be03e99
Erik Scow <erikscow@gmail.com>
2020-07-17 18:38:02 -0700

    Added middleware for validating id and action/project.

--- routers/actionRouter.js
+++ routers/actionRouter.js
@@ -3,3 +3,38 @@ const express = require('express')
 const router = express.Router()
 
 module.exports = router
+
+function validateId(req, res, next) {
+    const { id } = req.params
+    actionData
+      .get(id)
+      .then(action => {
+        if(action){
+          req.action = action
+          next()
+        } else {
+          res.status(400).json({ message: "invalid action id" })
+        }
+      })
+      .catch(error => {
+        console.log(`${req.method} could not complete: `, error)
+        res.status(500).json({ error: "could not retrieve from database"})
+      })
+  
+  }
+
+  function validateAction(req, res, next) {
+    if(!req.body){
+        res.status(400).json({ message: "missing action data" })
+    } else if (!req.body.project_id){
+        res.status(400).json({ message: "missing required project_id field" })
+    } else if (!req.body.description){
+        res.status(400).json({ message: "missing required description field" })
+    } else if (req.body.description.length > 128){
+        res.status(400).json({ message: "description length is too long"})
+    } else if (!req.body.notes){
+        res.status(400).json({ messate: "missing required notes field"})
+    } else {
+      next()
+    }
+  }
--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -3,3 +3,34 @@ const express = require('express')
 const router = express.Router()
 
 module.exports = router
+
+function validateId(req, res, next) {
+    const { id } = req.params
+    projectData
+      .get(id)
+      .then(project => {
+        if(project){
+          req.project = project
+          next()
+        } else {
+          res.status(400).json({ message: "invalid project id" })
+        }
+      })
+      .catch(error => {
+        console.log(`${req.method} could not complete: `, error)
+        res.status(500).json({ error: "could not retrieve from database"})
+      })
+  
+  }
+
+function validateProject(req, res, next) {
+    if(!req.body){
+        res.status(400).json({ message: "missing project data" })
+    } else if (!req.body.name){
+        res.status(400).json({ message: "missing required name field" })
+    } else if (!req.body.description){
+        res.status(400).json({ message: "missing required description field" })
+    } else {
+      next()
+    }
+  }
